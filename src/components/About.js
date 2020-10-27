ceb1ea4d810fd6150ec051139e5df7523e9d4501
Parth Makadiya <parth.makadiya@neptune-ubi.com>
2020-10-27 16:34:21 +0530

    some refactoring

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default function About() {
     return (
-        <div>
+        <div className="container" style={{textAlign: 'center'}}>
             <h3>Why Another Todo App?</h3>
+            <p>Just learning React</p>
         </div>
     )
 }
